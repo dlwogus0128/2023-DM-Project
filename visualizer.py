c6a18e7fda22ba962341dbf6f6a224f0646ca7fe
Jong <jm991014@gmail.com>
2023-12-06 03:52:07 +0900

    네트워크 반전

--- visualizer.py
+++ visualizer.py
@@ -6,7 +6,6 @@ from matplotlib import font_manager
 
 def visualize_results(svd_components, tfidf_results):
     graph = nx.Graph()
-    print("nxversion: "+ nx.__version__)
 
     for idx, topic in enumerate(svd_components):
         topic_keywords = [(tfidf_results[i], topic[i].round(5)) for i in topic.argsort()[:-10 - 1:-1]]
@@ -15,13 +14,13 @@ def visualize_results(svd_components, tfidf_results):
 
         for i in range(len(keywords) - 1):
             for j in range(i + 1, len(keywords)):
-                weight = abs(topic[i] - topic[j])
+                weight = abs(topic_keywords[i][1] - topic_keywords[j][1])  # TF-IDF 값의 차이를 이용하여 가중치 계산
                 graph.add_edge(keywords[i], keywords[j], weight=weight)
 
     # 단어 중요도 계산해 배치 (.,.)
 
     centrality = nx.betweenness_centrality(graph)
-    sorted_centrality = {k: v for k, v in sorted(centrality.items(), key=lambda item: item[1], reverse=True)}
+    sorted_centrality = {k: v for k, v in sorted(centrality.items(), key=lambda item: item[1], reverse=False)}
     pos = {node: (centrality[node], idx) for idx, node in enumerate(sorted_centrality.keys())}
 
     # 폰트 설정 및 레이아웃
